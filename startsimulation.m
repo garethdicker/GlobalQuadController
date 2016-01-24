7bb31e9e249b2a3711d360ee6bbd18ee75981c0e
gareth <gareth.dicker@hotmail.com>
2016-01-24 11:00:45 -0500

    resolve merge

--- startsimulation.m
+++ startsimulation.m
@@ -17,7 +17,6 @@ clc;
 % Initialize global parameters.
 initparams;
 
-<<<<<<< HEAD
 % Monte Carlo struct
 [MonteCarlo] = initmontecarlo;
 
@@ -27,13 +26,6 @@ dt = 1 / 200; % time step (Hz)
 iterations = 10;
 
 for i = 1:iterations
-=======
-% Define starting pose and twist parameters.
-IC.posn     = [0; 0; 2]; % world frame position (meters) 
-IC.linVel   = [0; 0; 0]; % world frame velocity (m / s)
-IC.angVel   = [30; 30; 4]; % body rates (radians / s)
-IC.attEuler = [pi; 0; 0]; % [roll; pitch; yaw] (radians)
->>>>>>> 8bce4fa04278d501efa4ea630ace06a150f017d3
 
     % Define starting pose and twist parameters.
     IC.posn     = [0; 0; 0]; % world frame position (meters) 
@@ -42,32 +34,15 @@ IC.attEuler = [pi; 0; 0]; % [roll; pitch; yaw] (radians)
     r = 2*pi*(rand-0.5)
     IC.attEuler = [r; 0; 0]; % [roll; pitch; yaw] (radians)
 
-<<<<<<< HEAD
     % Initialize state and its derivative.
     [state, stateDeriv] = initstate(IC);
-=======
-endTime = 1;  % seconds
-dt = 1 / 200; % time step (Hz)
->>>>>>> 8bce4fa04278d501efa4ea630ace06a150f017d3
 
-    % Define control types using pose and twist struct types.
-    Control = initcontrol;
-
-<<<<<<< HEAD
     % Update pose and twist structs from state and its derivative.
     [Pose, Twist] = updatekinematics(state, stateDeriv);
-=======
-% Initialize propeller state
-PropState = initpropstate([-1000; 1000; -1000; 1000]);
-
-% Update pose and twist structs from state and its derivative.
-[Pose, Twist] = updatekinematics(state, stateDeriv);
->>>>>>> 8bce4fa04278d501efa4ea630ace06a150f017d3
 
     % Initialize history for plotting and visualizing simulation.
     Hist = inithist(state, stateDeriv, Pose, Twist, Control);
 
-<<<<<<< HEAD
     % Simulation
     for i = 0 : dt : endTime - dt
         
@@ -98,36 +73,6 @@ PropState = initpropstate([-1000; 1000; -1000; 1000]);
 
     end
     MonteCarlo = [MonteCarlo; Hist];
-=======
-%% Simulation
-for i = 0 : dt : endTime - dt
-    
-    % Set control input for recovery controller.
-    % TODO: make world frame not body
-    Control.acc = [0; 1; 9.81];
-    
-    [recoveryStage] = checkrecoverystage(Pose, Twist);
-    
-%     [Control] = computedesiredacceleration(Control, Pose, Twist, recoveryStage);
-    
-    % Compute control outputs
-    [Control] = controllerrecovery(dt, Pose, Twist, Control, Hist);
-        
-    % Propagate dynamics.
-    options = odeset('RelTol',1e-3);
-    [tODE,stateODE] = ode45(@(tODE, stateODE) ...
-    dynamicsystem(tODE, stateODE, dt, Control.rpm, PropState.rpm),[i i+dt], Hist.states(:,end), options);
-    [stateDeriv, PropState] = dynamicsystem(tODE(end), stateODE(end,:)', dt, Control.rpm, PropState.rpm);
-    state = stateODE(end,:)';
-    t = tODE(end,:)- dt;
-    
-    % Update kinematic variables from dynamics output.
-    [Pose, Twist] = updatekinematics(state, stateDeriv);
-
-    % Update history.
-    Hist = updatehist(Hist, t, state, stateDeriv, Pose, Twist, Control);
-    
->>>>>>> 8bce4fa04278d501efa4ea630ace06a150f017d3
 end
 %% Display Plots
 % plotbodyrates(Hist.times, Hist.controls, Hist.twists);
